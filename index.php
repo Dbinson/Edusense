a69a2ed785ed07cf62295e4b5ba922bf1b428137
Dbinson <63998997+Dbinson@users.noreply.github.com>
2021-11-27 13:34:42 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>dashboard</title>
+    <title>Home</title>
 
 
     <!--Bootstrap CSS-->
@@ -13,7 +13,6 @@
     <!--FontAwesome CSS-->
     <link rel="stylesheet" href="css/all.min.js">
 
-    </script>
 </head>
 <body>
     <!--Start Navbar-->
