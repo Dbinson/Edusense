2e262e20f7ebf5ea9e1fb3215aadf3efb399e2e1
Dbinson <dsobinsonza0908@gmail.com>
2022-04-27 13:59:59 +0530

    updated add faculty

--- enrollProcess.php
+++ enrollProcess.php
@@ -3,19 +3,31 @@
     if(!isset($_SESSION)){
         session_start();
     }
-    $isEnrolled = false;
-    if(isset($_SESSION['is_login'])){
+
+    $isEnroll = 0;
+
+    if(isset($_SESSION['islogged'])){
         if(isset($_POST['id'])){
-            $sql = "INSERT INTO enroll(subject_id,student_id) 
-            VALUES ('".$_POST['id']."','".$_SESSION['studentid']."') ";
-            $query = mysqli_query($conn, $sql);
-            if($query){
-                $isEnrolled = true;
+
+            //check if already enrolled
+            $sql2 = "SELECT * FROM enroll
+                WHERE student_id = '".$_SESSION['student_id']."' AND subject_id = '".$_POST['id']."'";
+            $query2 = mysqli_query($conn, $sql);
+            if(mysqli_num_rows($query2)==0){
+                //enroll
+                $sql = "INSERT INTO enroll(subject_id,student_id) 
+                VALUES ('".$_POST['id']."','".$_SESSION['student_id']."') ";
+                $query = mysqli_query($conn, $sql);
+                if($query){
+                    $isEnrolled = 1;
+                }
+            }else{
+                $isEnroll = -1;
             }
+            
         }   
-    }else{
-        header("Location: login.php?enroll=1");
-        exit();
     }
+
+    echo $isEnroll;
 
 ?>
