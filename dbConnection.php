4f0c3d243de5527099d381bbf3b7188d95356722
Dally Serrao <dallyserrao@gmail.com>
2022-04-27 12:37:21 +0530

    change css

--- dbConnection.php
+++ dbConnection.php
@@ -2,7 +2,7 @@
 $db_host = "localhost";
 $db_user = "root";
 $db_password = "";
-$db_name = "Edusense";
+$db_name = "edusense";
 
 // Create Connection
 $conn = new mysqli($db_host, $db_user, $db_password, $db_name);
@@ -14,4 +14,3 @@ if(!$conn) {
 // else {
 //  echo"connected";
 // }
-?>
