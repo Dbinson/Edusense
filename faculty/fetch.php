75d288ea31cd39cb2f1edca6ff0b1ba2e12bfbd2
Dbinson <dsobinsonza0908@gmail.com>
2022-05-10 11:56:56 +0530

    updated assignment

--- faculty/fetch.php
+++ faculty/fetch.php
@@ -5,7 +5,7 @@
     $array = array();
 
     if(isset($_POST['id']) && $_POST['request']=="assignmentUpdate"){
-        $sql = 'SELECT  FROM assignment WHERE assignment_id="' .$_POST['id'].'"';
+        $sql = 'SELECT * FROM assignment WHERE assignment_id="' .$_POST['id'].'"';
         $query = mysqli_query($conn , $sql);
         while($row = mysqli_fetch_array($query)){
             $array[] = $row;
--- faculty/removeRequest.php
+++ faculty/removeRequest.php
@@ -3,19 +3,19 @@
 if (isset($_SERVER["HTTP_REFERER"])) {
     header("Location: " . $_SERVER["HTTP_REFERER"]);
 }
-include("dbConnection.php");
+include("../dbConnection.php");
 
 $delete_id = $_GET['del_id'];
 $r = $_GET['r'];
 
-// if($r=='f'){
-    $sql = mysqli_query($conn,"DELETE FROM assignment WHERE assignment_d=".$delete_id);
+if($r=='f'){
+    $sql = mysqli_query($conn,"DELETE FROM assignment WHERE assignment_id='".$delete_id."'");
 
     if($sql){
         exit();
     }
     
-// }
+}
 
 
 ?>
