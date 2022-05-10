54e8e3ca8ff664058b1e626fa125d204529e44a4
baviya <baviyahprasad@gmail.com>
2022-05-10 12:58:28 +0530

    update

--- admin/modals/updateStudentModal.php
+++ admin/modals/updateStudentModal.php
@@ -47,6 +47,7 @@
             </div>
         </div>
         <div class="modal-footer">
+        <span id="successMsg"></span>
             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
             <button type="submit" class="btn btn-primary">update</button>
         </div>
