8173e027a8ae3fd0329bac6e89afcea21f403f6f
baviya <baviyahprasad@gmail.com>
2022-05-15 09:28:47 +0530

    changessss

--- dbConnection.php
+++ dbConnection.php
@@ -2,7 +2,7 @@
 $db_host = "localhost";
 $db_user = "root";
 $db_password = "";
-$db_name = "edusense-final";
+$db_name = "edu-final";
 
 // Create Connection
 $conn = new mysqli($db_host, $db_user, $db_password, $db_name);
