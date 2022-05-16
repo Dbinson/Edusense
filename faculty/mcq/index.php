d936e360fa3e1e68f42a09cab288678afff33fcb
Dally Serrao <dallyserrao@gmail.com>
2022-05-17 01:31:46 +0530

    validation mcq

--- faculty/mcq/index.php
+++ faculty/mcq/index.php
@@ -27,14 +27,12 @@ if (!isset($_SESSION['is_login'])) {
 
           <div class="mb-3">  
                     <label for="subjectId" class="form-label">Subject</label>
-                            <select class="form-select" required name="subjectId"  pattern="[a-zA-Z ]*$" required id="subjectId"  oninvalid="alert('Incorrect Name.')"
+                            <select class="form-select" required name="subjectId"  required id="subjectId"
                                 aria-label="Default select example">
                             <option selected>Select subject </option>
                     <?php
                         include_once('../../dbConnection.php');    
-                        $sql=mysqli_query($conn,"SELECT * from enroll 
-                        LEFT JOIN subject ON enroll.subject_id=subject.subject_id 
-                        WHERE faculty_id='".$_SESSION["faculty_id"]."';");
+                        $sql=mysqli_query($conn,"SELECT * from subject");
                         while($result=mysqli_fetch_assoc($sql)){
                             echo "<option value=".$result['subject_id'].">".$result['name']. "  ".$result['class']."</option>";
                         }
@@ -50,9 +48,7 @@ if (!isset($_SESSION['is_login'])) {
                             <option selected>Select Student </option>
                     <?php
                         include_once('../../dbConnection.php');    
-                        $sql=mysqli_query($conn,"SELECT student.student_id,student.stud_name from enroll 
-                        LEFT JOIN student ON enroll.student_id=student.student_id 
-                        WHERE enroll.faculty_id='".$_SESSION["faculty_id"]."';");
+                        $sql=mysqli_query($conn,"SELECT student_id,stud_name from student");
                         while($result=mysqli_fetch_assoc($sql)){
                             echo "<option value=".$result['student_id'].">".$result['stud_name']."</option>";
                         }
@@ -63,25 +59,25 @@ if (!isset($_SESSION['is_login'])) {
           <div class="input-box">
             <div class=" mb-3">
               <label for="question1" class="form-label">Q1</label>
-              <input type="text" class="form-control" placeholder="Enter Question" required name="question1" required id="question1" />
+              <input type="text" class="form-control" placeholder="Enter Question" required name="question1" pattern="[A-Za-z0-9]{1,100}" oninvalid="alert('Invalid')" required id="question1" />
             </div>
           
             <div class=" mb-3">
               <label for="question1" class="form-label">Marks</label>
-              <input type="number" class="form-control" placeholder="Enter Marks" required name="marks1" required id="marks1" />
+              <input type="number" class="form-control" placeholder="Enter Marks" required name="marks1" pattern="[A-Za-z0-9]{1,100}" oninvalid="alert('Invalid')" required id="marks1" />
             </div>
 
             <div class="form-check mb-3">
-              <input class="form-check-input" type="radio" required name="question1Option" required id="question1Option1" />
+              <input class="form-check-input" type="radio" required name="question1Option" pattern="[A-Za-z0-9]{1,100}" oninvalid="alert('Invalid')" required id="question1Option1" />
               <input type="text" placeholder="Option 1" name="question10option1Text" id="question1Option1Text" />
             </div>
             
             <div class="form-check mb-3">
-              <input class="form-check-input" type="radio" required name="question1Option" required id="question1Option2" />
+              <input class="form-check-input" type="radio" required name="question1Option" pattern="[A-Za-z0-9]{1,100}" oninvalid="alert('Invalid')" required id="question1Option2" />
               <input type="text" placeholder="OPtion 2" name="question10option2Text" id="question1Option2Text" />
             </div>
             <div class="form-check mb-3">
-              <input class="form-check-input" type="radio" required name="question1Option" required id="question1Option3" />
+              <input class="form-check-input" type="radio" required name="question1Option" pattern="[A-Za-z0-9]{1,100}" oninvalid="alert('Invalid')"required id="question1Option3" />
               <input type="text" placeholder="Option 3" name="question10option3Text" id="question1Option3Text" />
             </div>
           </div>
@@ -89,24 +85,24 @@ if (!isset($_SESSION['is_login'])) {
           <div class="input-box">
             <div class="mb-3">
               <label for="exampleInputPassword1" class="form-label">Q2</label>
-              <input type="text" class="form-control" required placeholder="Enter Question" required name="question2" required id="question2" />
+              <input type="text" class="form-control" required placeholder="Enter Question" required name="question2" pattern="[A-Za-z0-9]{1,100}" oninvalid="alert('Invalid')" required id="question2" />
             </div>
 
             <div class=" mb-3">
               <label for="question1" class="form-label">Marks</label>
-              <input type="number" class="form-control" placeholder="Enter Marks" required name="marks2" required id="marks2" />
+              <input type="number" class="form-control" placeholder="Enter Marks" required name="marks2" pattern="[A-Za-z0-9]{1,100}" oninvalid="alert('Invalid')" required id="marks2" />
             </div>
 
             <div class="form-check mb-3">
-              <input class="form-check-input" type="radio" required name="question2Option" required id="question2Option1" />
+              <input class="form-check-input" type="radio" required name="question2Option"  pattern="[A-Za-z0-9]{1,100}" oninvalid="alert('Invalid')"  required id="question2Option1" />
               <input type="text" placeholder="option 1" name="question20option1Text" required id="question2Option1Text" />
             </div>
             <div class="form-check mb-3">
-              <input class="form-check-input" type="radio" required name="question2Option" required id="question2Option2" />
+              <input class="form-check-input" type="radio" required name="question2Option"  pattern="[A-Za-z0-9]{1,100}" oninvalid="alert('Invalid')" required id="question2Option2" />
               <input type="text" placeholder="option 2" name="question20option2Text" required id="question2Option2Text" />
             </div>
             <div class="form-check mb-3">
-              <input class="form-check-input" type="radio" required name="question2Option" required id="question2Option3" />
+              <input class="form-check-input" type="radio" required name="question2Option"   pattern="[A-Za-z0-9]{1,100}" oninvalid="alert('Invalid')"  required id="question2Option3" />
               <input type="text" placeholder="option 3" name="question20option3Text" required id="question2Option3Text" />
             </div>
           </div>
@@ -114,7 +110,7 @@ if (!isset($_SESSION['is_login'])) {
           <div class="input-box">
             <div class="mb-3">
               <label for="exampleInputPassword1" class="form-label">Q3</label>
-              <input type="text" class="form-control" placeholder="Enter Question" required name="question3" required id="question3" />
+              <input type="text" class="form-control" placeholder="Enter Question" required name="question3" pattern="[A-Za-z0-9]{1,100}" oninvalid="alert('Invalid')"required id="question3" />
             </div>
 
             <div class=" mb-3">
@@ -124,22 +120,22 @@ if (!isset($_SESSION['is_login'])) {
 
             <div class="form-check mb-3">
               <input class="form-check-input" type="radio" name="question3Option" required id="question3Option3" />
-              <input type="text" placeholder="option 1" name="question30option1Text" required id="question3Option1Text" />
+              <input type="text" placeholder="option 1" name="question30option1Text" pattern="[A-Za-z0-9]{1,100}" oninvalid="alert('Invalid')" required id="question3Option1Text" />
             </div>
             <div class="form-check mb-3">
               <input class="form-check-input" type="radio" name="question3Option" required id="question3Option3" />
-              <input type="text" placeholder="option 2" name="question30option2Text" required id="question3Option2Text" />
+              <input type="text" placeholder="option 2" name="question30option2Text" pattern="[A-Za-z0-9]{1,100}" oninvalid="alert('Invalid')" required id="question3Option2Text" />
             </div>
             <div class="form-check mb-3">
               <input class="form-check-input" type="radio" required name="question3Option" required id="question3Option3" />
-              <input type="text" placeholder="option 3" name="question30option3Text" required id="question3Option3Text" />
+              <input type="text" placeholder="option 3" name="question30option3Text" pattern="[A-Za-z0-9]{1,100}" oninvalid="alert('Invalid')" required id="question3Option3Text" />
             </div>
           </div>
 
           <div class="input-box">
             <div class="mb-3">
               <label for="exampleInputPassword1" class="form-label">Q4</label>
-              <input type="text" class="form-control" placeholder="Enter Question" name="question4" required id="question4" />
+              <input type="text" class="form-control" placeholder="Enter Question" name="question4" pattern="[A-Za-z0-9]{1,100}" oninvalid="alert('Invalid')" required id="question4" />
             </div>
 
             <div class=" mb-3">
@@ -189,7 +185,7 @@ if (!isset($_SESSION['is_login'])) {
           <div class="input-box">
             <div class="mb-3">
               <label for="exampleInputPassword1" class="form-label">Q6</label>
-              <input type="text" class="form-control" placeholder="Enter Question" required name="question6" required id="question6" />
+              <input type="text" class="form-control" placeholder="Enter Question" required name="question6" pattern="[A-Za-z0-9]{1,100}" oninvalid="alert('Invalid')" required id="question6" />
             </div>
 
             <div class=" mb-3">
@@ -213,7 +209,7 @@ if (!isset($_SESSION['is_login'])) {
           <div class="input-box">
             <div class="mb-3">
               <label for="exampleInputPassword1" class="form-label">Q7</label>
-              <input type="text" class="form-control" placeholder="Enter Question" required name="question7" required id="question7" />
+              <input type="text" class="form-control" placeholder="Enter Question" required name="question7" pattern="[A-Za-z0-9]{1,100}" oninvalid="alert('Invalid')" required id="question7" />
             </div>
 
             <div class=" mb-3">
@@ -223,15 +219,15 @@ if (!isset($_SESSION['is_login'])) {
 
             <div class="form-check mb-3">
               <input class="form-check-input" type="radio" required name="question7Option" required id="question7Option7" />
-              <input type="text" placeholder="option 1" name="question70option1Text" required id="question7Option1Text" />
+              <input type="text" placeholder="option 1" name="question70option1Text" pattern="[A-Za-z0-9]{1,100}" oninvalid="alert('Invalid')" required id="question7Option1Text" />
             </div>
             <div class="form-check mb-3">
               <input class="form-check-input" type="radio" required name="question7Option" required id="question7Option7" />
-              <input type="text" placeholder="option 2" name="question70option2Text" required id="question7Option2Text" />
+              <input type="text" placeholder="option 2" name="question70option2Text" pattern="[A-Za-z0-9]{1,100}" oninvalid="alert('Invalid')" required id="question7Option2Text" />
             </div>
             <div class="form-check mb-3">
               <input class="form-check-input" type="radio"required name="question7Option" required id="question7Option7" />
-              <input type="text" placeholder="option 3" name="question70option3Text" required id="question7Option3Text" />
+              <input type="text" placeholder="option 3" name="question70option3Text" pattern="[A-Za-z0-9]{1,100}" oninvalid="alert('Invalid')" required id="question7Option3Text" />
             </div>
           </div>
 
@@ -239,25 +235,25 @@ if (!isset($_SESSION['is_login'])) {
           <div class="input-box">
             <div class="mb-3">
               <label for="exampleInputPassword1" class="form-label">Q8</label>
-              <input type="text" class="form-control" placeholder="Enter Question" required name="question8" required id="question8" />
+              <input type="text" class="form-control" placeholder="Enter Question" required name="question8" pattern="[A-Za-z0-9]{1,100}" oninvalid="alert('Invalid')" required id="question8" />
             </div>
 
             <div class=" mb-3">
               <label for="question1" class="form-label">Marks</label>
-              <input type="number" class="form-control" placeholder="Enter Marks" required name="marks8" required id="marks8" />
+              <input type="number" class="form-control" placeholder="Enter Marks" required name="marks8"  required id="marks8" />
             </div>
 
             <div class="form-check mb-3">
               <input class="form-check-input" type="radio" required name="question8Option" required id="question8Option8" />
-              <input type="text" placeholder="option 1" name="question80option1Text" required id="question8Option1Text" />
+              <input type="text" placeholder="option 1" name="question80option1Text" pattern="[A-Za-z0-9]{1,100}" oninvalid="alert('Invalid')" required id="question8Option1Text" />
             </div>
             <div class="form-check mb-3">
               <input class="form-check-input" type="radio" required name="question8Option" required id="question8Option8" />
-              <input type="text" placeholder="option 2" name="question80option2Text" required id="question8Option2Text" />
+              <input type="text" placeholder="option 2" name="question80option2Text" pattern="[A-Za-z0-9]{1,100}" oninvalid="alert('Invalid')" required id="question8Option2Text" />
             </div>
             <div class="form-check mb-3">
               <input class="form-check-input" type="radio" required name="question8Option" required id="question8Option8" />
-              <input type="text" placeholder="option 3" name="question80option3Text" required id="question8Option3Text" />
+              <input type="text" placeholder="option 3" name="question80option3Text" pattern="[A-Za-z0-9]{1,100}" oninvalid="alert('Invalid')" required id="question8Option3Text" />
             </div>
           </div>
 
@@ -265,54 +261,53 @@ if (!isset($_SESSION['is_login'])) {
           <div class="input-box">
             <div class="mb-3">
               <label for="exampleInputPassword1" class="form-label">Q9</label>
-              <input type="text" class="form-control" placeholder="Enter Question" required name="question9" required id="question9" />
+              <input type="text" class="form-control" placeholder="Enter Question" required name="question9" pattern="[A-Za-z0-9]{1,100}" oninvalid="alert('Invalid')" required id="question9" />
             </div>
 
             <div class=" mb-3">
               <label for="question1" class="form-label">Marks</label>
-              <input type="number" class="form-control" placeholder="Enter Marks" required name="marks9" required id="marks9" />
+              <input type="number" class="form-control" placeholder="Enter Marks" required name="marks9"  required id="marks9" />
             </div>
 
             <div class="form-check mb-3">
               <input class="form-check-input" type="radio" required name="question9Option" required id="question9Option9" />
-              <input type="text" placeholder="option 1" name="question90option1Text" required id="question9Option1Text" />
+              <input type="text" placeholder="option 1" name="question90option1Text" pattern="[A-Za-z0-9]{1,100}" oninvalid="alert('Invalid')" required id="question9Option1Text" />
             </div>
             <div class="form-check mb-3">
               <input class="form-check-input" type="radio" required name="question9Option" required id="question9Option9" />
-              <input type="text" placeholder="option 2" name="question90option2Text" required id="question9Option2Text" />
+              <input type="text" placeholder="option 2" name="question90option2Text"  pattern="[A-Za-z0-9]{1,100}" oninvalid="alert('Invalid')" required id="question9Option2Text" />
             </div>
             <div class="form-check mb-3">
               <input class="form-check-input" type="radio" required name="question9Option" required id="question9Option9" />
-              <input type="text" placeholder="option 3" name="question90option3Text" required id="question9Option3Text" />
+              <input type="text" placeholder="option 3" name="question90option3Text" pattern="[A-Za-z0-9]{1,100}" oninvalid="alert('Invalid')" required id="question9Option3Text" />
             </div>
           </div>
           <div class="input-box">
             <div class="mb-3">
               <label for="exampleInputPassword1" class="form-label">Q10</label>
-              <input type="text" class="form-control" placeholder="Enter Question" required name="question10" required id="question10" />
+              <input type="text" class="form-control" placeholder="Enter Question" required name="question10" pattern="[A-Za-z0-9]{1,100}" oninvalid="alert('Invalid')" required id="question10" />
             </div>
 
             <div class=" mb-3">
               <label for="question1" class="form-label">Marks</label>
-              <input type="number" class="form-control" placeholder="Enter Marks" required name="marks10" required id="marks10" />
+              <input type="number" class="form-control" placeholder="Enter Marks" required name="marks10"  required id="marks10" />
             </div>
 
             <div class="form-check mb-3">
               <input class="form-check-input" type="radio" required name="question10Option" required id="question10Option10" />
-              <input type="text" placeholder="option 1" name="question100option1Text" required id="question10Option1Text" />
+              <input type="text" placeholder="option 1" name="question100option1Text" pattern="[A-Za-z0-9]{1,100}" oninvalid="alert('Invalid')" required id="question10Option1Text" />
             </div>
             <div class="form-check mb-3">
               <input class="form-check-input" type="radio" required name="question10Option" required id="question10Option10" />
-              <input type="text" placeholder="option 2" name="question100option2Text" required id="question10Option2Text" />
+              <input type="text" placeholder="option 2" name="question100option2Text"  pattern="[A-Za-z0-9]{1,100}" oninvalid="alert('Invalid')" required id="question10Option2Text" />
             </div>
             <div class="form-check mb-3">
               <input class="form-check-input" type="radio" required name="question10Option" required id="question10Option10" />
-              <input type="text" placeholder="option 3" name="question100option3Text" required id="question10Option3Text" />
+              <input type="text" placeholder="option 3" name="question100option3Text" pattern="[A-Za-z0-9]{1,100}" oninvalid="alert('Invalid')" required id="question10Option3Text" />
             </div>
           </div>
 
           <button type="submit" class="btn btn-primary ">Add</button>
-          <span id="successmsgg"></span>
         </form>
       </div>
     </div>
